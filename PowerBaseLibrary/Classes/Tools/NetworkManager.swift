0da9e2b3d0823f09c34812e8963143eef7ceb170
邹程 <zoucheng@live.cn>
2020-11-03 10:57:23 +0800

    优化

--- PowerBaseLibrary/Classes/Tools/NetworkManager.swift
+++ PowerBaseLibrary/Classes/Tools/NetworkManager.swift
@@ -5,15 +5,15 @@
 //  Created by 邹程 on 2019/9/2.
 //
 
-import Foundation
 import Alamofire
+import Foundation
 
 public class NetworkManager {
     // shared instance
     public static let shared = NetworkManager()
-    
-    public let reachabilityManager = NetworkReachabilityManager(host: "www.baidu.com")
-    
+
+    public let reachabilityManager = NetworkReachabilityManager(host: Bundle.main.infoDictionary?["ReachabilityDomain"] as? String ?? "www.baidu.com")
+
     public func startNetworkReachabilityObserver() {
         reachabilityManager?.listener = { status in
             switch status {
@@ -21,25 +21,25 @@ public class NetworkManager {
                 DEBUGLOG("The network is reachable over the WiFi connection")
                 CONNECTION_TYPE = .ethernetOrWiFi
                 NotificationCenter.default.post(name: Notification.Name(rawValue: N_APP_NETWORK_AVAILABLE), object: nil)
-                
+
             case .reachable(.wwan):
                 DEBUGLOG("The network is reachable over the WWAN connection")
                 CONNECTION_TYPE = .wwan
                 NotificationCenter.default.post(name: Notification.Name(rawValue: N_APP_NETWORK_AVAILABLE), object: nil)
-                
+
             case .notReachable:
                 DEBUGLOG("The network is not reachable")
                 NotificationCenter.default.post(name: Notification.Name(rawValue: N_APP_NETWORK_UNAVAILABLE), object: nil)
-                
+
             case .unknown:
                 DEBUGLOG("It is unknown whether the network is reachable")
             }
         }
-        
+
         if reachabilityManager?.isReachable == false {
             NotificationCenter.default.post(name: Notification.Name(rawValue: N_APP_NETWORK_UNAVAILABLE), object: nil)
         }
-        
+
         // start listening
         reachabilityManager?.startListening()
     }
